58042d4c35b6ea050050d93682591f101549d475
susanta <susanta@susanta-laptop.(none)>
2010-10-18 13:43:45 +0530

    first commit

---
+++ test/src/com/susanta/test/Test.java
@@ -0,0 +1,14 @@
+package com.susanta.test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: susanta
+ * Date: 18 Oct, 2010
+ * Time: 1:09:06 PM
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
